b1cec2db0ae1fe3356a765d878b8dd38adc38d15
Hao Truong <truongdinhquochao@gmail.com>
2025-03-28 16:24:32 +0700

    11

--- src/TeduBlog.Data/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SampleBlog.Data
-{
-    public class Class1
-    {
-
-    }
-}
--- src/TeduBlog.WebApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SampleBlog.WebApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
