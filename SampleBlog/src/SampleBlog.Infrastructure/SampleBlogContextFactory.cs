7b1896eaa256abb26ea62a5fce34a6ce6bf9245f
Jay <truongdinhquochao@gmail.com>
2025-03-29 15:10:14 +0700

    Feature/add migration (#3)
    * add configuration
    
    * migration database ver 1

---
+++ SampleBlog/src/SampleBlog.Infrastructure/SampleBlogContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleBlog.Infrastructure
+{
+    public class SampleBlogContextFactory : IDesignTimeDbContextFactory<SampleBlogContext>
+    {
+        public SampleBlogContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+             .SetBasePath(Directory.GetCurrentDirectory())
+             .AddJsonFile("appsettings.json")
+             .Build();
+            
+            var builder = new DbContextOptionsBuilder<SampleBlogContext>();
+            builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            return new SampleBlogContext(builder.Options);
+        }
+    }
+}
